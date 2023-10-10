dd065982c50cc230296b283dd72954dd91c8cfd1
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-10-10 14:18:38 -0400

    Solid plugins working state. Logging is working.

--- app/src/main/java/com/ebsolutions/eventsadminservice/controllers/HealthController.java
+++ app/src/main/java/com/ebsolutions/eventsadminservice/controllers/HealthController.java
@@ -2,12 +2,16 @@ package com.ebsolutions.eventsadminservice.controllers;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import lombok.extern.slf4j.Slf4j;
 
-@Controller("/test")
-public class TestController {
+import java.time.LocalDateTime;
+
+@Slf4j
+@Controller("/health")
+public class HealthController {
 
     @Get(produces = "text/plain")
     public String index() {
-        return "Example Response";
+        return "Service is alive and the time is " + LocalDateTime.now();
     }
 }
