298b1f8684c234fccbd90fe94c2a8d50c9e6ade3
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-03-22 14:48:44 -0400

    MVB and CL

--- app/src/main/java/com/ebsolutions/eventsadminservice/config/WebSecurityConfig.java
+++ app/src/main/java/com/ebsolutions/eventsadminservice/config/WebSecurityConfig.java
@@ -2,11 +2,14 @@ package com.ebsolutions.eventsadminservice.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +18,9 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        .cors(cors -> cors
+            .configurationSource(request -> this.createCorsConfiguration())
+        )
         .authorizeHttpRequests(authorization ->
             authorization
                 .requestMatchers("/actuator/health")
@@ -27,4 +33,15 @@ public class WebSecurityConfig {
     return http.build();
   }
 
+  public CorsConfiguration createCorsConfiguration() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+    configuration.setAllowCredentials(true);
+
+    configuration.setAllowedOrigins(
+        Collections.singletonList("http://localhost:3000")); // Replace with your origin
+
+    return configuration;
+  }
 }
