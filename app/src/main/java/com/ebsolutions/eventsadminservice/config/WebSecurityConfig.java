ead87b83d6eca7c6d19b856c78b06a18f067ee4b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-03-29 17:03:40 -0400

    Got things working it seems AuthZ and CORS wise

--- app/src/main/java/com/ebsolutions/eventsadminservice/config/WebSecurityConfig.java
+++ app/src/main/java/com/ebsolutions/eventsadminservice/config/WebSecurityConfig.java
@@ -1,46 +1,26 @@
 package com.ebsolutions.eventsadminservice.config;
 
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.web.cors.CorsConfiguration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
-  private static final List<String> ALLOWED_ORIGINS =
-      List.of(
-          "http://localhost:3000",
-          "https://events.otterandcow.com",
-          // House or gym
-          "64.98.122.48",
-          // House or gym
-          "96.10.245.210",
-          // Tavern
-          "107.12.84.23"
-      );
 
-  //  @Bean
-//  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//    httpSecurity
-//        .cors(cors -> cors
-//            .configurationSource(request -> this.corsConfiguration())
-//        )
-//        .authorizeHttpRequests(
-//            auth ->
-//                auth.
-//                    requestMatchers("/establishments")
-//                    .permitAll()
-//                    .anyRequest()
-//                    .permitAll());
-//    return httpSecurity.build();
-//  }
+  private final CorsConfig corsConfig;
 
-  //  @Bean
-  CorsConfiguration corsConfiguration() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    configuration.setAllowedOrigins(ALLOWED_ORIGINS);
-    return configuration;
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(req -> req.anyRequest().permitAll())
+        .cors(c -> c.configurationSource(corsConfig));
+    return httpSecurity.build();
   }
 }
